d8511fd54efe3fa7b07ae756b54e74bfa6c58fbe
themagicdev3351 <themagicdev3351@gmail.com>
2024-09-30 15:33:30 +0530

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,15 @@ module.exports = {
         historyApiFallback: true,
         proxy: {
             '/api': {
-                target: 'http://ec2-3-108-218-11.ap-south-1.compute.amazonaws.com:8080',
+                target: 'http://ec2-3-108-218-11.ap-south-1.compute.amazonaws.com:8080', // Your backend API
                 changeOrigin: true,
+                secure: false, // Set to false if your backend is using HTTP
+                pathRewrite: { '^/api': '' }, // Remove '/api' from the request path if necessary
             },
         },
+        // If you want to run on HTTPS
+        https: true, // Set to true if you want to serve your React app over HTTPS during development
+        port: 3000,
     },
+
 };
